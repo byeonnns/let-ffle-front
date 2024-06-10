96e0a33e01bd75e41461d2a935e19668cf38ff59
ryan <ljwryan@gmail.com>
2024-06-10 20:13:38 +0900

    Refactor: Board에 필요없는 컴포넌트 삭제

--- src/router/Board.js
+++ src/router/Board.js
@@ -1,14 +1,8 @@
 import BoardList from '../views/Board/BoardList.vue'
 import Write from '../views/Board/Write.vue'
-import Announcement from '../views/Board/Announcement.vue'
-import BoardDetail from '../views/Board/BoardDetail.vue'
 import BoardComment from '../views/Board/BoardComment.vue'
 
 const routes = [
-    {
-        path: '/Board/Announcement',
-        component: () => import(/* webpackChunkName: "Board" */ '@/views/Board/Announcement')
-    },
     {
         path: '/Board/Write',
         component: () => import(/* webpackChunkName: "Board" */ '@/views/Board/Write')
@@ -16,10 +10,6 @@ const routes = [
     {
         path: '/Board/BoardList',
         component: () => import(/* webpackChunkName: "Board" */ '@/views/Board/BoardList')
-    },
-    {
-        path: '/Board/BoardDetail',
-        component: () => import(/* webpackChunkName: "Board" */ '@/views/Board/BoardDetail')
     },
     {
         path: '/Board/BoardComment',
