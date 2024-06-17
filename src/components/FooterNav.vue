00ca69fa5306a1f26f2f7d1b13cc3af92bd0eef9
ryan <ljwryan@gmail.com>
2024-06-17 15:39:52 +0900

    Refactor: FooterNav 링크 수정

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -3,10 +3,10 @@
         <div class="position-absolute w-100 bottom-0 bg-white row">
             <ul class="d-flex list-unstyled text-center align-content-center my-1">
                 <RouterLink to="/" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-house-door d-block"></i>HOME</li></RouterLink>
-                <RouterLink to="/Raffle/Main" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-box2-heart d-block"></i>RAFFLE</li></RouterLink>
+                <RouterLink to="/Raffle" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-box2-heart d-block"></i>RAFFLE</li></RouterLink>
                 <RouterLink to="/Notice" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-star d-block"></i>NOTICE</li></RouterLink>
                 <RouterLink to="/Board/BoardList" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-people d-block"></i>COMMUNITY</li></RouterLink>
-                <RouterLink to="/Member/Mypage" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-person-standing d-block"></i>MY</li></RouterLink>
+                <RouterLink to="/Member" class="flex-grow-1"><li style="font-size:0.6rem"><i class="bi bi-person-standing d-block"></i>MY</li></RouterLink>
             </ul>
         </div>
     </div>
