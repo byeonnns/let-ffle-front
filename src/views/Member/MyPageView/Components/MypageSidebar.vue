5cfc4f8d512165c0b920b837f5bd7359612d1ef9
GIM-YOUNG-JOO <explicit13@naver.com>
2024-06-19 11:49:25 +0900

    Refactor : 사이드바 수정 3트

--- src/views/Member/MyPageView/Components/MypageSidebar.vue
+++ src/views/Member/MyPageView/Components/MypageSidebar.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <nav class="d-flex flex-column">
-            <RouterLink to="/Member/MyPage" class="text-center text-nowrap h3 mb-5 no-animation" style="font-family: NanumSquareNeo; font-weight: 700">마이페이지</RouterLink>
-            <h4>래플 정보</h4>
-            <div class="d-flex flex-column bg-light mb-5">
+            <RouterLink to="/Member/MyPage" class="text-center text-nowrap h4 mb-4 no-animation" style="font-family: NanumSquareNeo; font-weight: 700">마이페이지</RouterLink>
+            <h5>래플 정보</h5>
+            <div class="d-flex flex-column bg-light mb-4">
                 <RouterLink to="/Member/MyPage/MyEntryList" class="ps-2 mb-2">응모 내역</RouterLink>
                 <RouterLink to="/Member/MyPage/WinningList" class="ps-2 mb-2">당첨 내역</RouterLink>
                 <RouterLink to="/Member/MyPage/LikeList" class="ps-2">찜 목록</RouterLink>
             </div>
-            <h4>커뮤니티 관리</h4>
-            <div class="d-flex flex-column bg-light mb-5">
+            <h5>커뮤니티 관리</h5>
+            <div class="d-flex flex-column bg-light mb-4">
                 <RouterLink to="/Member/MyPage/MyBoardList" class="ps-2 mb-2">내 게시글</RouterLink>
                 <RouterLink to="/Member/MyPage/MyCommentList" class="ps-2">내 댓글</RouterLink>
             </div>
-            <h4>나의 정보</h4>
-            <div class="d-flex flex-column bg-light mb-5">
+            <h5>나의 정보</h5>
+            <div class="d-flex flex-column bg-light">
                 <RouterLink to="/Member/MyPage/MyBerryHistory" class="ps-2 mb-2">베리 변동 내역</RouterLink>
                 <RouterLink to="/Member/MyPage/UpdateMember" class="ps-2 mb-2">회원 정보 수정</RouterLink>
                 <RouterLink to="/Member/MyPage/InquiryList" class="ps-2">문의 하기</RouterLink>
@@ -27,6 +27,10 @@
 </script>
 
 <style scoped>
+div{
+    font-family: NanumSquareNeo;
+}
+
 a:not(.no-animation) {
     border-left: 2px solid rgba(243, 117, 81, 0);
 }
