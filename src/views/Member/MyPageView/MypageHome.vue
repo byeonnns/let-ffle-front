ced90c2ab5149046849e60944b8c027fa2533fea
GIM-YOUNG-JOO <explicit13@naver.com>
2024-06-19 16:43:45 +0900

    refactor : 마이페이지 대시보드 수정중

--- src/views/Member/MyPageView/MypageHome.vue
+++ src/views/Member/MyPageView/MypageHome.vue
@@ -1,24 +1,57 @@
 <template>
     <div>
-        <div>
-            <div class="d-flex mb-5">
-                <div class="card custom-card">
-                    <div class="card-body mt-3 ms-3 flex-column d-flex align-items-start">
-                        <div class="mt-3 ms-3">
-                            <div>
-                                <strong>gekx0m</strong>
-                            </div>
-                            <p>asd@naver.com</p>
+        <div class="d-flex flex-column">
+            <div style="border-bottom: 3px solid #F37551;">
+                <h3>Dashboard</h3>
+            </div>
+            <div class="card flex-grow-1 my-4">
+                <div class="card-body d-flex pb-2">
+                    <RouterLink to="/Member/MyPage/UpdateMember" class="align-content-center">
+                        <div class="btn text-center px-3 rounded-2">
+                            <strong>신우호 [닉네임]</strong>
+                            <h6 class="mt-2 m-0">asd@naver.com</h6>
                         </div>
-                        <div class="position-absolute top-50 end-0 translate-middle-y me-4">
-                            <RouterLink to="/Member/MyPage/UpdateMember" class="btn btn-lg border"
-                                style="color: white;">내 정보 수정</RouterLink>
+                    </RouterLink>
+                    <div class="flex-grow-1">
+                        <h4 class="text-center">일일 베리 퀘스트</h4>
+                        <div class="mt-3">
+                            <div class="d-flex text-center">
+                                <div class="col">
+                                    <h3>매일 매일 접속하기</h3>
+                                    <h5>Complete</h5>
+                                </div>
+                                <div class="col">
+                                    <h3>래플 응모하기</h3>
+                                    <h5>Complete</h5>
+                                </div>
+                                <div class="col">
+                                    <h3>래플&미션 올 클리어</h3>
+                                    <h5>Incomplete</h5>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <hr class="border-3 opacity-100 mt-0 mb-2" style="border-color: #F37551;" />
-            <h4 class="text-center my-3">래플 현황</h4>
+            <div class="flex-grow-1">
+                <div class="d-flex text-center">
+                    <div class="col">
+                        <h3>총 응모 수</h3>
+                        <h5>1</h5>
+                    </div>
+                    <div class="col">
+                        <h3>참여 진행 중</h3>
+                        <h5>1</h5>
+                    </div>
+                    <div class="col">
+                        <h3>종료</h3>
+                        <h5>1</h5>
+                    </div>
+                </div>
+            </div>
+            <hr class="border-3 opacity-100 my-3" style="border-color: #F37551;" />
+            <h4 class="text-center my-3">최근 응모 래플</h4>
             <table class="table text-center">
                 <thead>
                     <tr>
@@ -61,28 +94,7 @@
                     </tr>
                 </tbody>
             </table>
-
-            <br>
-            <table class="w-100 text-center">
-                <thead>
-                    <tr>
-                        <th>래플 이름</th>
-                        <th>응모 기간</th>
-                        <th>응모 상품</th>
-                        <th>바로가기</th>
-                    </tr>
-                </thead>
-                <tbody class="table table-group-divider">
-                    <tr>
-                        <td>아이폰 뿌리는 이벤트</td>
-                        <td>2024.01.01 ~ 2024.02.02</td>
-                        <td>아이폰 11Pro</td>
-                        <td>
-                            <RouterLink to="/Raffle"><button class="btn w-50">래플 바로가기</button></RouterLink>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <hr class="border-3 opacity-100 my-3" style="border-color: #F37551;" />
         </div>
     </div>
 </template>
@@ -91,24 +103,6 @@
 </script>
 
 <style scoped>
-.custom-card {
-    height: 150px;
-    width: 100%;
-}
-
-.card-body {
-    margin-top: 10px;
-}
-
-.card-body .btn {
-    font-size: 12px;
-}
-
-.btn {
-    background-color: #F37551;
-    color: white;
-}
-
 .circle-border {
     width: 60px;
     height: 60px;
--- src/views/Member/MyPageView/UpdateMember.vue
+++ src/views/Member/MyPageView/UpdateMember.vue
@@ -26,7 +26,7 @@
                         </div>
                         <div class="w-50 position-relative">
                             <div style="align-content: end;">
-                                <input v-model="testModel" type="text" placeholder="Kosa" class="input w-50">
+                                <input type="email" placeholder="Kosa" class="input w-50">
                             </div>
                             <hr class="p-0 m-0 w-100">
                             <button class="btn btn-outline-light btn-sm"
@@ -85,7 +85,6 @@
                                 </RouterLink>
                             </div>
                         </div>
-                        <h1> 테스트 : {{ testModel }}</h1>
                     </div>
                 </div>
             </div>
@@ -231,8 +230,6 @@
 import RaffleModal from '@/components/RaffleModal.vue';
 import { ref } from 'vue'
 import { VueDaumPostcode } from 'vue-daum-postcode';
-
-const testModel = ref(null);
 
 const PWModal = ref(null);
 const PWCModal = ref(null);
