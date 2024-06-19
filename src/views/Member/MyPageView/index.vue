99021dba11b266ad312806da9b9f36b0cfb5c4d7
ShinWooHo <kua040847@gmail.com>
2024-06-19 16:50:19 +0900

    confilict 수정

--- src/views/Member/MyPageView/index.vue
+++ src/views/Member/MyPageView/index.vue
@@ -1,10 +1,9 @@
 <template>
     <div>
-        <div class="d-flex container">
+        <div class="d-flex container-lg">
             <MypageNavBar />
-            <div class="flex-grow-1">
+            <div class="flex-grow-1 ps-5">
                 <RouterView />
-                
             </div>
         </div>
     </div>
@@ -13,80 +12,6 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import MypageNavBar from '@/views/Member/MyPageView/Components/MypageSidebar.vue';
-
-
-
-
 </script>
 
-<style scoped>
-.nav-item>.nav-link {
-    color: black;
-    font-size: 15px;
-}
-
-.circle-border {
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    border: 1px solid black;
-    overflow: hidden;
-}
-
-.circle-border img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-
-.position-absolute>.btn {
-    margin-right: 10px;
-
-}
-
-.btn {
-    color: gray;
-}
-
-.ms-1 .card-texts {
-    font-size: 13px;
-}
-
-.ms-1 .fw-bold {
-    font-size: 14px;
-}
-
-.router-link-active {
-    color: #FF5C35;
-}
-
-a {
-    text-decoration: none;
-    color: black;
-}
-
-/*  */
-.circle-border {
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    border: 1px solid black;
-    overflow: hidden;
-}
-
-/* .circle-border img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-
-.position-absolute>.btn {
-    margin-right: 10px;
-
-}
-
-.btn {
-    background-color: #F37551;
-    color: white;
-} */
-</style>
+<style scoped></style>
