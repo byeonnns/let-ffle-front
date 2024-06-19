6aa38ee58661eb91c197a58b8d8484014df24151
ryan <ljwryan@gmail.com>
2024-06-19 09:02:50 +0900

    Refactor: CreateForm input type 간격 벌림

--- src/views/Admin/Components/CreateForm.vue
+++ src/views/Admin/Components/CreateForm.vue
@@ -10,21 +10,21 @@
                     </div>
                 </div>
 
-                <div class="form-group row">
+                <div class="form-group row mt-4">
                     <label for="btitle" class="col-sm-2 col-form-label">제목</label>
                     <div class="col-sm-10">
                         <input id="btitle" type="text" class="form-control" />
                     </div>
                 </div>
 
-                <div class="form-group row mt-3">
+                <div class="form-group row mt-4">
                     <label for="bcontent" class="col-sm-2 col-form-label">내용</label>
                     <div class="col-sm-10">
                         <textarea id="bcontent" type="text" class="form-control" style="height:250px;"></textarea>
                     </div>
                 </div>
 
-                <div class="form-group row mt-3">
+                <div class="form-group row mt-4">
                     <label for="battach" class="col-sm-2 col-form-label">첨부파일</label>
                     <div class="col-sm-10">
                         <input id="battach" type="file" class="form-control-file" ref="battach" />
