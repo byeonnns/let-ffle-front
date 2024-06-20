3d6a1f3358f6b22d348fd4f36b4187f57592c881
ryan <ljwryan@gmail.com>
2024-06-20 12:20:36 +0900

    Refactor: joinview 유효성 검사 해결

--- src/views/Member/JoinView.vue
+++ src/views/Member/JoinView.vue
@@ -2,65 +2,66 @@
     <div class="container-lg">
         <div class="d-flex justify-content-center">
             <!--아이디(이메일), 비밀번호, 이름, 주민번호, 전화번호, 주소를 입력받는다.-->
-            <form @submit.prevent="handleSubmit">
-                <div class="d-flex flex-column" :style="responsiveSize">
-                    <p class="text-center fw-bold fst-italic" style="font-size:50px">Let-<span
-                            style="color:#FF5C35">FFle</span></p>
+            <!-- <form @submit.prevent="handleSubmit"> -->
+            <div class="d-flex flex-column" :style="responsiveSize">
+                <p class="text-center fw-bold fst-italic" style="font-size:50px">Let-<span
+                        style="color:#FF5C35">FFle</span></p>
 
-                    <label for="memail mb-2">이메일 주소*</label>
-                    <div class="d-flex justify-content-between">
-                        <input id="memail" type="email" class="border-0 border-bottom flex-grow-1 input"
-                            v-model="member.mid">
-                        <button class="btn text-white btn-outline-light btn-sm">중복 확인</button>
-                    </div>
-                    <p style="color: red;">{{ checkMid }}</p>
-
-                    <label for="mpassword" class="mt-5 mb-2">비밀번호*</label>
-                    <input id="mpassword" type="password" class="border-0 border-bottom input"
-                        v-model="member.mpassword">
-                    <p style="color: red;">{{ checkMpassword }}</p>
-
-                    <label for="mname" class="mt-5 mb-2 input">이름</label>
-                    <input id="mname" type="text" class="border-0 border-bottom input" v-model="member.mname">
-                    <p style="color: red;">{{ checkMname }}</p>
-
-                    <label for="mnickname" class="mt-5 mb-2">닉네임</label>
-                    <div class="d-flex justify-content-between">
-                        <input id="mnickname" type="text" class="border-0 border-bottom flex-grow-1 input"
-                            v-model="member.mnickname">
-                        <button class="btn text-white btn-outline-light btn-sm">중복 확인</button>
-                    </div>
-
-                    <label for="mte" class="mt-5 mb-2">전화번호</label>
-                    <input id="mphone" type="text" class="border-0 border-bottom input" v-model="member.mphone">
-                    <p style="color: red;"> {{ checkMphone }}</p>
-
-                    <label for="maddress" class="mt-5 mb-2">주소</label>
-                    <div class="d-flex justify-content-between">
-                        <input v-model="zonecode" id="maddress" type="text"
-                            class="border-0 border-bottom text-center flex-grow-1 input" placeholder="우편번호">
-                        <button class="btn text-white btn-outline-light btn-sm">우편번호 찾기</button>
-                    </div>
-                    <input v-model="address" type="text" class="border-0 border-bottom mt-3 input" placeholder="주소">
-                    <input v-model="addressDetail" type="text" class="border-0 border-bottom mt-3 input"
-                        placeholder="상세주소">
-                    <button class="btn text-white btn-lg mt-5 w-100">가입하기</button>
-
+                <label for="memail mb-2">이메일 주소*</label>
+                <div class="d-flex justify-content-between">
+                    <input id="memail" type="email" class="border-0 border-bottom flex-grow-1 input"
+                        v-model="member.mid" @input="midCheck(onbtn)">
+                    <button class="btn text-white btn-outline-light btn-sm">중복 확인</button>
                 </div>
-                <RaffleModal ref="postcodeModal">
-                    <template v-slot:modalHeader>
-                        주소 검색
-                    </template>
-                    <template v-slot:modalBody>
-                        <VueDaumPostcode :animation=true :max-suggest-items="3" :theme='{
-                            textColor: "#000000", //기본 글자색
-                            postcodeTextColor: "#000000", //우편번호 글자색
-                            emphTextColor: "#FF5C35", //강조 글자색
-                            outlineColor: "#FF5C35" //테두리
-                        }' v-if="postcodeMount" @complete="addressSearched" />
-                    </template>
-                </RaffleModal>
-            </form>
+                <p style="color: red;">{{ checkMid }}</p>
+
+                <label for="mpassword" class="mt-5 mb-2">비밀번호*</label>
+                <input id="mpassword" type="password" class="border-0 border-bottom input" v-model="member.mpassword"
+                    @input="mpasswordCheck(onbtn)">
+                <p style="color: red;">{{ checkMpassword }}</p>
+
+                <label for="mname" class="mt-5 mb-2 input">이름</label>
+                <input id="mname" type="text" class="border-0 border-bottom input" v-model="member.mname"
+                    @input="mnameCheck(onbtn)">
+                <p style="color: red;">{{ checkMname }}</p>
+
+                <label for="mnickname" class="mt-5 mb-2">닉네임</label>
+                <div class="d-flex justify-content-between">
+                    <input id="mnickname" type="text" class="border-0 border-bottom flex-grow-1 input"
+                        v-model="member.mnickname">
+                    <button class="btn text-white btn-outline-light btn-sm">중복 확인</button>
+                </div>
+
+                <label for="mte" class="mt-5 mb-2">전화번호</label>
+                <input id="mphone" type="text" class="border-0 border-bottom input" v-model="member.mphone"
+                    @input="mphoneCheck(onbtn)">
+                <p style="color: red;"> {{ checkMphone }}</p>
+
+                <label for="maddress" class="mt-5 mb-2">주소</label>
+                <div class="d-flex justify-content-between">
+                    <input v-model="zonecode" id="maddress" type="text"
+                        class="border-0 border-bottom text-center flex-grow-1 input" placeholder="우편번호">
+                    <button class="btn text-white btn-outline-light btn-sm">우편번호 찾기</button>
+                </div>
+                <input v-model="address" type="text" class="border-0 border-bottom mt-3 input" placeholder="주소">
+                <input v-model="addressDetail" type="text" class="border-0 border-bottom mt-3 input" placeholder="상세주소">
+                <button class="btn text-white btn-lg mt-5 w-100" :class="ispass ? '' : 'disabled'">가입하기</button>
+
+            </div>
+            <RaffleModal ref="postcodeModal">
+                <template v-slot:modalHeader>
+                    주소 검색
+                </template>
+                <template v-slot:modalBody>
+                    <VueDaumPostcode :animation=true :max-suggest-items="3" :theme='{
+                        textColor: "#000000", //기본 글자색
+                        postcodeTextColor: "#000000", //우편번호 글자색
+                        emphTextColor: "#FF5C35", //강조 글자색
+                        outlineColor: "#FF5C35" //테두리
+                    }' v-if="postcodeMount" @complete="addressSearched" />
+                </template>
+            </RaffleModal>
+            <!-- </form> -->
         </div>
     </div>
 </template>
@@ -75,6 +76,7 @@ const checkMid = ref(null);
 const checkMpassword = ref(null);
 const checkMname = ref(null);
 const checkMphone = ref(null);
+const ispass = ref(false);
 // 
 
 const postcodeModal = ref(null);
@@ -127,54 +129,115 @@ const member = ref({
     maddress: ""
 });
 
-function handleSubmit() {
+// function handleSubmit() {
 
-    var total = true;
+//     var total = true;
 
+//     var midPattern = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+//     var userMid = midPattern.test(member.value.mid);
+//     if (userMid) {
+//         checkMid.value = "";
+//     } else if (!userMid) {
+//         total = false;
+//         checkMid.value = "올바르지 않은 양식입니다";
+//     }
+
+//     var mpasswordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+//     var userPassword = mpasswordPattern.test(member.value.mpassword);
+//     if (userPassword) {
+//         checkMpassword.value = ""
+//     } else if (!userPassword) {
+//         total = false;
+//         checkMpassword.value = "올바르지 않은 양식입니다"
+
+//     }
+
+//     var mnamePattern = /^[가-힣]+$/;
+//     var userMname = mnamePattern.test(member.value.mname);
+//     if (userMname) {
+//         checkMname.value = "";
+//     } else if(!userMname) {
+//         total = false;
+//         checkMname.value = "올바르지 않는 양식입니다."
+//     }
+
+
+
+//     var mphonePattern = /^010-\d{3,4}-\d{4}$/;
+//     var userPhone = mphonePattern.test(member.value.mphone);
+//     if (userPhone) {
+//         checkMphone.value =" "
+//     } else if ((!userPhone)) {
+//         total = false;
+//         checkMphone.value = "올바르지 않는 양식입니다."
+//     }
+
+//     if (total) {
+//      member.submit
+//      }
+
+// }
+
+
+// 유효성 검사
+let userMid;
+let userPassword;
+let userMname;
+let userPhone;
+
+const midCheck = (onbtn) => {
     var midPattern = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
-    var userMid = midPattern.test(member.value.mid);
-    if (userMid) {
+    userMid = midPattern.test(member.value.mid);
+    if (!userMid) {
+        checkMid.value = "이메일 주소를 정확히 입력해주세요."
+    } else {
         checkMid.value = "";
-    } else if (!userMid) {
-        total = false;
-        checkMid.value = "올바르지 않은 양식입니다";
     }
-
-    var mpasswordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
-    var userPassword = mpasswordPattern.test(member.value.mpassword);
-    if (userPassword) {
-        checkMpassword.value = ""
-    } else if (!userPassword) {      
-        total = false;
-        checkMpassword.value = "올바르지 않은 양식입니다"
-
-    }
-
-    var mnamePattern = /^[가-힣]+$/;
-    var userMname = mnamePattern.test(member.value.mname);
-    if (userMname) {
-        checkMname.value = "";
-    } else if(!userMname) {
-        total = false;
-        checkMname.value = "올바르지 않는 양식입니다."
-    }
-
-
-
-    var mphonePattern = /^010-\d{3,4}-\d{4}$/;
-    var userPhone = mphonePattern.test(member.value.mphone);
-    if (userPhone) {
-        checkMphone.value =" "
-    } else if ((!userPhone)) {
-        total = false;
-        checkMphone.value = "올바르지 않는 양식입니다."
-    }
-
-    // if (total) {
-    // member.submit
-    // }
+    onbtn();
 
 }
+
+const mpasswordCheck=(onbtn)=>{
+    var mpasswordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+     userPassword = mpasswordPattern.test(member.value.mpassword);
+    if (!userPassword) {
+        checkMpassword.value="비밀번호를 입력해주세요"
+    } else {
+        checkMpassword.value=""
+    }
+    onbtn();
+}
+
+const mnameCheck = (onbtn) => {
+    var mnamePattern = /^[가-힣]{2,4}$/;
+    userMname = mnamePattern.test(member.value.mname);
+    if (!userMname) {
+    checkMname.value="이름을 입력해주세요"
+    } else {
+    checkMname.value=""
+    }
+    onbtn();
+}
+
+const mphoneCheck = (onbtn) => {
+    var mphonePattern = /^010-\d{3,4}-\d{4}$/;
+    userPhone = mphonePattern.test(member.value.mphone);
+    if (!userPhone) {
+        checkMphone.value="전화번호를 입력해주세요"
+    } else {
+        checkMphone.value=""
+    }
+    onbtn();
+}
+function onbtn(){
+    if (userMid && userPassword && userMname && userPhone) {
+        ispass.value="true"
+    } else {
+        ispass.value="false"
+    }
+}
+
+
 
 </script>
 
