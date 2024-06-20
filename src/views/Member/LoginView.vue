806915b3833cebce02a4e2fbc190b9fb014c4f95
GIM-YOUNG-JOO <explicit13@naver.com>
2024-06-20 11:40:10 +0900

    Feat : 로그인 유효성 검사 완료

--- src/views/Member/LoginView.vue
+++ src/views/Member/LoginView.vue
@@ -3,18 +3,25 @@
         <div class="d-flex justify-content-center">
             <!--아이디(이메일), 비밀번호, 이름, 주민번호, 전화번호, 주소를 입력받는다.-->
             <div class="d-flex flex-column" :style="responsiveSize">
-                <p class="text-center fst-italic" style="font-size:50px; font-weight: 900">Let-<span style="color:#FF5C35">FFle</span></p>
+                <p class="text-center fst-italic" style="font-size:50px; font-weight: 900">Let-<span
+                        style="color:#FF5C35">FFle</span></p>
                 <label for="memail">이메일 주소</label>
-                <input v-model="memail" type="email" class="border-0 border-bottom input" @input="emailCheck">
+                <input v-model="memail" type="email" class="border-0 border-bottom input" @input="emailCheck(onBtn)">
                 <p style="color:#FF5C35">{{ emailError }}</p>
                 <label for="mpassword" class="mt-3">비밀번호</label>
-                <input v-model="mpassword" type="password" class="border-0 border-bottom input" @input="passwordCheck">
+                <input v-model="mpassword" type="password" class="border-0 border-bottom input" @input="passwordCheck(onBtn)">
                 <p style="color: #FF5C35;">{{ passwordError }}</p>
-                <button class="btn text-white btn-lg mt-4" :class=" isPass ? '' : 'disabled'">로그인</button>
+                <button class="btn text-white btn-lg mt-4" :class="isPass ? '' : 'disabled'">로그인</button>
                 <div class="row mt-3 text-center">
-                    <div class="col-4 border-end"><RouterLink to="/join">이메일 가입</RouterLink></div>
-                    <div class="col-4"><RouterLink to="/FindId">이메일 찾기</RouterLink></div>
-                    <div class="col-4 border-start"><RouterLink to="/FindPassword">비밀번호 찾기</RouterLink></div>
+                    <div class="col-4 border-end">
+                        <RouterLink to="/join">이메일 가입</RouterLink>
+                    </div>
+                    <div class="col-4">
+                        <RouterLink to="/FindId">이메일 찾기</RouterLink>
+                    </div>
+                    <div class="col-4 border-start">
+                        <RouterLink to="/FindPassword">비밀번호 찾기</RouterLink>
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,21 +36,31 @@ const mpassword = ref('');
 const passwordError = ref('');
 const emailError = ref('');
 const isPass = ref(false);
-
 const emailRegExp = new RegExp("[a-z0-9]+@[a-z]+\\.[a-z]{2,3}");
-const emailCheck = () => {
-    if(!emailRegExp.test(memail.value)){
+
+const emailCheck = (onBtn) => {
+    if (!emailRegExp.test(memail.value)) {
         emailError.value = "이메일 주소를 정확히 입력해주세요."
     } else {
         emailError.value = "";
     }
+    onBtn();
 }
 
-const passwordCheck = () => {
-    if(mpassword.value.length == 0) {
+const passwordCheck = (onBtn) => {
+    if (mpassword.value.length == 0) {
         passwordError.value = '비밀번호를 입력해주세요.';
     } else {
         passwordError.value = '';
+    }
+    onBtn();
+}
+
+function onBtn() {
+    if (emailRegExp.test(memail.value) && mpassword.value.length > 0) {
+        isPass.value = true;
+    } else {
+        isPass.value = false;
     }
 }
 
@@ -65,11 +82,11 @@ window.addEventListener('resize', handleResize);
 </script>
 
 <style scoped>
-.input{
+.input {
     outline: none;
 }
 
 p {
-    font-size:12px;
+    font-size: 12px;
 }
 </style>
