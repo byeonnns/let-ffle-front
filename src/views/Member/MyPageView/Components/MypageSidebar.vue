3e6a3bfb4b919b7792eec88b8006bb9603156932
byeonnns <tjdwns3823@naver.com>
2024-06-21 09:10:02 +0900

    Refactor: Mypage sidebar 디자인 수정

--- src/views/Member/MyPageView/Components/MypageSidebar.vue
+++ src/views/Member/MyPageView/Components/MypageSidebar.vue
@@ -3,22 +3,21 @@
         <nav class="d-flex flex-column">
             <RouterLink to="/Member/MyPage" class="text-center text-nowrap h4 mb-4 no-animation fw-bold">마이 페이지</RouterLink>
             <h5>래플 정보</h5>
-            <div class="d-flex flex-column bg-light mb-4">
-                <RouterLink to="/Member/MyPage/MyEntryList" class="ps-2 mb-2">응모 내역</RouterLink>
-                <RouterLink to="/Member/MyPage/WinningList" class="ps-2 mb-2">당첨 내역</RouterLink>
-                <RouterLink to="/Member/MyPage/LikeList" class="ps-2">찜 목록</RouterLink>
+            <div class="d-flex flex-column bg-light" style="margin-bottom: 30px;">
+                <RouterLink to="/Member/MyPage/MyEntryList" class="ps-2 mt-3">응모 내역</RouterLink>
+                <RouterLink to="/Member/MyPage/WinningList" class="ps-2 mt-2">당첨 내역</RouterLink>
+                <RouterLink to="/Member/MyPage/LikeList" class="ps-2 mt-2 mb-3">찜 목록</RouterLink>
             </div>
             <h5>커뮤니티 관리</h5>
-            <div class="d-flex flex-column bg-light mb-4">
-                <RouterLink to="/Member/MyPage/MyBoardList" class="ps-2 mb-2">내 게시글</RouterLink>
-                <RouterLink to="/Member/MyPage/MyCommentList" class="ps-2">내 댓글</RouterLink>
+            <div class="d-flex flex-column bg-light" style="margin-bottom: 30px;">
+                <RouterLink to="/Member/MyPage/MyBoardList" class="ps-2 mt-3">내 게시글</RouterLink>
+                <RouterLink to="/Member/MyPage/MyCommentList" class="ps-2 mt-2 mb-3">내 댓글</RouterLink>
             </div>
             <h5>나의 정보</h5>
             <div class="d-flex flex-column bg-light">
-                <RouterLink to="/Member/MyPage/MyBerryHistory" class="ps-2 mb-2">베리 변동 내역</RouterLink>
-                <RouterLink to="/Member/MyPage/UpdateMember" class="ps-2 mb-2">회원 정보 수정</RouterLink>
-                <RouterLink to="/Member/MyPage/InquiryList" class="ps-2">문의 하기</RouterLink>
-                <RouterLink to="/Member/MyPage/UpdateMember2" class="ps-2">정보 수정2</RouterLink>
+                <RouterLink to="/Member/MyPage/MyBerryHistory" class="ps-2 mt-3">베리 변동 내역</RouterLink>
+                <RouterLink to="/Member/MyPage/UpdateMember" class="ps-2 mt-2">회원 정보 수정</RouterLink>
+                <RouterLink to="/Member/MyPage/InquiryList" class="ps-2 mt-2 mb-3">문의 하기</RouterLink>
             </div>
         </nav>
     </div>
