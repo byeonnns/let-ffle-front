794700119fbe61c6b1ef8ab59f4b0f67ce7ffc60
ShinWooHo <kua040847@gmail.com>
2024-07-02 22:12:06 +0900

    Refactor : Mypage 당첨조회 바인딩

--- src/apis/RaffleAPI.js
+++ src/apis/RaffleAPI.js
@@ -6,6 +6,10 @@ function myEntryList() {
     return axios.get("/raffle/getRaffleDetailList")
 }
 
+function myWinnerList() {
+    return axios.get("/raffle/getWinnerDetailList");
+}
+
 // 첨부 다운로드
 function raffleAttachDownload(rno) {
     // Path Variable로 데이터 전송, JSON 타입을 받는 것이 아니므로 타입을 추가로 지정
@@ -14,5 +18,6 @@ function raffleAttachDownload(rno) {
 
 export default {
     myEntryList,
+    myWinnerList,
     raffleAttachDownload
 }
