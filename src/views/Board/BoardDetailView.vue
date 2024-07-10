a21c7c862e01a867de6e42bea03cebfb3e37dda4
ryan <ljwryan@gmail.com>
2024-07-10 17:49:59 +0900

    Refactor: boardDetail 에서 오류난 부분 수정

--- src/views/Board/BoardDetailView.vue
+++ src/views/Board/BoardDetailView.vue
@@ -8,8 +8,8 @@
             <h2>[{{ board.bcategory }}] {{ board.btitle }}</h2>
             <div class="d-flex justify-content-between">
                 <div>
-                    <span class="me-3">{{ board.mid }}</span><i class="bi bi-clock me-2">{{ formatDate(board.bcreatedat) }}</i><i
-                        class="bi bi-eye">{{ board.bhitcount }}</i>
+                    <span class="me-3">{{ board.battachoname }}</span><i class="bi bi-clock me-2">{{ formatDate(board.bcreatedat)
+                        }}</i><i class="bi bi-eye">{{ board.bhitcount }}</i>
                 </div>
                 <div v-if="$store.state.mid === board.mid">
                     <input type="submit" class="btn btn-outline-light btn-sm me-2 rounded-0" value="수정"
